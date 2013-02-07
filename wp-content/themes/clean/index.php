664d6c3a5a958f5f8c0fd7520f7dc3c34e0642fb
Filip Nilsson <filipnils@outlook.com>
2013-02-07 17:03:04 +0100

    Allting i index.php

--- wp-content/themes/clean/index.php
+++ wp-content/themes/clean/index.php
@@ -1,9 +1,19 @@
 <!doctype html>
 <html>
 	<head>
-		<title></title>
+		<title><?php blog_info('name'); ?> | <?php wp_title(); ?></title>
+		<meta charset="UTF-8">
+
+		<link rel="stylesheet" href="<?php blog_info('stylesheet_url'); ?>">
+		<link rel="pingback" href="<?php blog_info('pingback_url'); ?>">
+
+		<?php wp_head(); ?>
 	</head>
 	<body>
-		
+		<div class="container">
+			<div class="header">
+				
+			</div>
+		</div>
 	</body>
 </html>

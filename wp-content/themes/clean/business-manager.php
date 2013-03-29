d9b574b7fa963eb3751cf6bffa12ccd370f126fa
Filip Nilsson <filipnils@outlook.com>
2013-03-29 15:34:10 +0100

    La till taxonomy

--- wp-content/themes/clean/business-manager.php
+++ wp-content/themes/clean/business-manager.php
@@ -25,4 +25,16 @@
         Types", "singular_label" => "Business Type", "rewrite"
         => true, "slug" => 'business-type'));
 
+	if (function_exists('add_theme_support')) {
+        add_theme_support('post-thumbnails');
+        set_post_thumbnail_size( 220, 150 );
+        add_image_size('storefront', 620, 270, true);
+}
+      add_action("admin_init", "business_manager_add_meta");
+      function business_manager_add_meta(){
+         add_meta_box("business-meta", "Business Options",
+          "business_manager_meta_options", "businesses",
+          "normal", "high");
+      }
+
 ?>
